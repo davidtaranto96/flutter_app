10e48c7547e4225d46fb0e830b311aaf08e1074b
David Taranto <42699001+davidtaranto96@users.noreply.github.com>
2021-08-17 08:35:20 -0300

    Se agrega el proyecto

---
+++ lib/User/ui/widgets/button_bar.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:travel_app/Place/ui/screens/add_place_screen.dart';
+import 'package:travel_app/User/bloc/bloc_user.dart';
+import 'circle_button.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ButtonsBar extends StatelessWidget {
+  UserBloc userBloc;
+
+  @override
+  Widget build(BuildContext context) {
+    final picker = ImagePicker();
+    userBloc = BlocProvider.of(context);
+
+    return Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: 0.0,
+            vertical: 10.0
+        ),
+        child: Row(
+          children: <Widget>[
+            //Cambiar la contraseña
+            CircleButton(true, Icons.vpn_key, 20.0, Color.fromRGBO(255, 255, 255, 0.6)
+                , () => {}),
+            //Añadiremos un nuevo lugar
+            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1),
+                    () {
+
+                      picker.getImage(source: ImageSource.camera).then((PickedFile image) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (BuildContext context) => AddPlaceScreen(image: File(image.path))));
+                      }).catchError((onError) => print(onError));
+
+                }),
+
+            //Cerrar Sesión
+            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+                    () => {
+                  userBloc.signOut()
+                }),
+
+          ],
+        )
+    );
+  }
+
+}
---
+++ lib/User/ui/widgets/profile_place.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/Place/model/place.dart';
+import 'package:travel_app/User/ui/widgets/profile_place_info.dart';
+import 'package:travel_app/User/ui/widgets/profile_place.dart';
+
+class ProfilePlace extends StatelessWidget {
+  Place place;
+
+  ProfilePlace(this.place);
+
+  @override
+  Widget build(BuildContext context) {
+    final photoCard = Container(
+      margin: EdgeInsets.only(top: 10.0, bottom: 70.0),
+      height: 220.0,
+      child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Image.network(
+            place.urlImage,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes
+                        : null,
+                  ));
+            },
+          )),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: Colors.red,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black38,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 5.0))
+          ]),
+    );
+
+    return Stack(
+      alignment: Alignment(0.0, 0.8),
+      children: <Widget>[photoCard, ProfilePlaceInfo(place)],
+    );
+  }
+}
